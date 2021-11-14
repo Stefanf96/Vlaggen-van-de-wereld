31873ac66cd039e05e7b7cf74be9e2fdef2ead5a
Stefan Filippo <54752442+Stefanf96@users.noreply.github.com>
2021-11-14 13:55:36 +0100

    added spam prevention on quizbuttons
    using disabled attribute for spam prevention

--- scripts/js/app.js
+++ scripts/js/app.js
@@ -1,35 +1,46 @@
 function quizzer() {
-  const flagId = getFlagCodes(flagJson);
-  createQuizSection(flagId, flagJson[flagId]);
+  const flagId = getFlagCodes(flagJson)
+  createQuizSection(flagId, flagJson[flagId])
 }
 function learning() {
-  const flagId = getFlagCodes(flagJson);
-  createLearnSection(flagId, flagJson[flagId]);
+  const flagId = getFlagCodes(flagJson)
+  createLearnSection(flagId, flagJson[flagId])
 }
-function quizBot(value, answer) {
-  if (value.toLowerCase() != answer.toLowerCase()) {
-    const alert = alertHelper(
-      `Thats Incorrect! The answer was <strong>${answer}</strong>`
-    );
-    contentArea.insertBefore(alert, row);
+function quizBot(value, answer, event) {
+  if (value.toLowerCase() == '') {
+    const alert = alertHelper(`You did not put a answer in`)
+    contentArea.insertBefore(alert, row)
     setTimeout(function () {
-      contentArea.removeChild(alert);
-      quizzer();
-    }, 2000);
-  } else if (value.toLowerCase() == answer.toLowerCase()) {
-    const success = successHelper(`Thats <strong>Correct!</strong>`);
-    contentArea.insertBefore(success, row);
+      contentArea.removeChild(alert)
+    }, 1500)
+  }
+  if (value.toLowerCase() != '' && value.toLowerCase() != answer.toLowerCase()) {
+    const alert = alertHelper(`Thats Incorrect! The answer was <strong>${answer}</strong>`)
+    contentArea.insertBefore(alert, row)
+    event.target.setAttribute('disabled', true)
     setTimeout(function () {
-      contentArea.removeChild(success);
-      quizzer();
-    }, 2000);
+      contentArea.removeChild(alert)
+      quizzer()
+      event.target.setAttribute('disabled', false)
+    }, 2000)
+  } else if (value.toLowerCase() != '' && value.toLowerCase() == answer.toLowerCase()) {
+    const success = successHelper(`Thats <strong>Correct!</strong>`)
+    contentArea.insertBefore(success, row)
+    event.target.setAttribute('disabled', true)
+    setTimeout(function () {
+      contentArea.removeChild(success)
+      quizzer()
+      event.target.setAttribute('disabled', false)
+    }, 2000)
   }
 }
-function skipQuestion(answer) {
-  const alert = alertHelper(`The answer was <strong>${answer}</strong>`);
-  contentArea.insertBefore(alert, row);
+function skipQuestion(answer, event) {
+  const alert = alertHelper(`The answer was <strong>${answer}</strong>`)
+  contentArea.insertBefore(alert, row)
+  event.target.setAttribute('disabled', true)
   setTimeout(function () {
-    contentArea.removeChild(alert);
-    quizzer();
-  }, 2000);
+    contentArea.removeChild(alert)
+    quizzer()
+    event.target.setAttribute('disabled', true)
+  }, 2000)
 }
--- scripts/js/vendor.js
+++ scripts/js/vendor.js
@@ -1,23 +1,12 @@
-﻿const section = document.getElementById('content');
-const flags = document.getElementById('flags');
-const contentArea = document.getElementById('content-area');
-const quizSection = document.getElementById('quiz');
-const row = document.getElementById('contentRow');
-let flagButton = document.getElementById('flagButton');
-let quizButton = document.getElementById('quizButton');
-let submitButton = document.getElementById('quizzerSubmit');
-let skipButton = document.getElementById('skip');
+﻿const section = document.getElementById('content')
+const flags = document.getElementById('flags')
+const contentArea = document.getElementById('content-area')
+const quizSection = document.getElementById('quiz')
+const row = document.getElementById('contentRow')
+let flagButton = document.getElementById('flagButton')
+let quizButton = document.getElementById('quizButton')
+let submitButton = document.getElementById('quizzerSubmit')
+const skipButton = document.getElementById('skipQuestionButton')
 
-flagButton.addEventListener('click', learning);
-quizButton.addEventListener('click', quizzer);
-
-if (typeof submitButton != 'undefined' || submitButton != null) {
-  let imageName =
-    document.getElementById('flagSprite') != null
-      ? document.getElementById('flagSprite').alt
-      : '';
-  let answer =
-    document.getElementById('quizzer') != null
-      ? document.getElementById('quizzer').value
-      : '';
-}
+flagButton.addEventListener('click', learning)
+quizButton.addEventListener('click', quizzer)
